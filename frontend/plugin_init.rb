f21e78956effb58529440b86459e1d334d7107fe
Payten Giles <payten.giles@gmail.com>
2015-05-13 11:59:43 +1000

    Instead of showing the 'level' on the record's indexed display string add a 'level' column to the search, advanced search and resource listing when archival objects are in mix.

--- frontend/plugin_init.rb
+++ frontend/plugin_init.rb
@@ -7,4 +7,65 @@ Rails.application.config.after_initialize do
     end
   end
 
+
+  ApplicationController.class_eval do
+    include SearchHelper
+
+    def add_level_column_to_results_listing
+      add_column("Level",
+                 proc {|record| I18n.t("enumerations.archival_record_level.#{record['level']}", :default => record['level'])},
+                 {
+                   :sortable => true,
+                   :sort_by => 'level'
+                 })
+    end
+  end
+
+
+  SearchController.class_eval do
+    alias_method :advanced_search_pre_component_levels, :advanced_search
+    def advanced_search
+      @component_levels_level_column_supported = true
+      advanced_search_pre_component_levels
+    end
+
+    alias_method :do_search_pre_component_levels, :do_search
+    def do_search
+      @component_levels_level_column_supported = true
+      do_search_pre_component_levels
+    end
+
+    private
+
+    # We need to add the column in render to be sure
+    # the column is added post initialising @search_data
+    # and prior to rendering the template.
+    def render(*args)
+      add_level_column_to_results_listing if @component_levels_level_column_supported && show_level_column?
+      super
+    end
+
+
+    def show_level_column?
+      (ASUtils.wrap(@search_data.types).empty? ||
+        @search_data.types.include?("archival_object")) &&
+      (!@search_data.filtered_terms? ||
+        @search_data[:criteria]["filter_term[]"].none?{|filter| filter =~ /primary_type/ && !(filter =~ /archival_object/)})
+    end
+  end
+
+
+  ResourcesController.class_eval do
+    alias_method :index_pre_component_levels, :index
+    def index
+      index_pre_component_levels
+      add_level_column_to_results_listing if show_level_column?
+    end
+
+    private
+
+    def show_level_column?
+      params[:include_components]==="true"
+    end
+  end
 end
